f84c7be5a934a20a39ef421d3b57ff9eeecba9dc
emil-KEA <emil33v4@stud.kea.dk>
2022-03-11 09:55:04 +0100

    med flere linjer

--- src/demo/Main.java
+++ src/demo/Main.java
@@ -5,5 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hej git");
         System.out.println("Hello");
+        System.out.println("Hello");
+        System.out.println("Hello");
     }
 }
